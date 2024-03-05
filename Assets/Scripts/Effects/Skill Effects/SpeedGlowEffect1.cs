fcb6dbf9e6382228cfa02978ac66ab8b40b07439
kjcabPL <kcabrera@profoundlogic.com>
2024-03-05 18:47:53 +0800

    - Add URP as the graphics pipeline for the game - Add a new material to be used for the player (and potentially the monsters/NPCs) - Update Glow Effects behavior to pingpong from regular to blue instead of using custom material URP for the meantime

--- Assets/Scripts/Effects/Skill Effects/SpeedGlowEffect1.cs
+++ Assets/Scripts/Effects/Skill Effects/SpeedGlowEffect1.cs
@@ -8,6 +8,7 @@ public class SpeedGlowEffect1 : Effects
     public GameObject target;
     public float GlowBrightness = 5.0f;
     private Renderer _renderer;
+    private Color baseColor, targetColor;
     private bool isGlowing = false;
 
     void Start()
@@ -17,15 +18,23 @@ public class SpeedGlowEffect1 : Effects
 
     void Update()
     {
+
         if (isGlowing) {
-            float glow = Mathf.PingPong(Time.time, GlowBrightness);
-            _renderer.material.SetFloat("_EmissionIntensity", glow);
+            // float glow = Mathf.PingPong(Time.time, GlowBrightness);
+            // _renderer.material.SetFloat("_EmissionIntensity", glow);
+            
+            // change the color to blue and back
+            float t = Mathf.PingPong(Time.time, 1);
+            Color newColor = Color.Lerp(baseColor, targetColor, t); // Interpolate between the base color and the target color
+            _renderer.material.color = newColor; // Set the new color
         }
     }
 
     public void startGlow()
     {
         _renderer = target.GetComponent<Renderer>();
+        baseColor = _renderer.material.color; // The base color of the sprite
+        targetColor = Color.blue; // The color you want to ping-pong to
         isGlowing = true;
     }
 
